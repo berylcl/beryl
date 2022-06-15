723195f13aa68635a8c26e30b2f8510ec26df494
gakii <gakiiberyl@gmail.com>
2022-06-15 12:42:54 +0300

    first commit

--- next day.c
+++ next day.c
@@ -1,1 +1,18 @@
-#
+#include <stdio.h>
+#include <stdlib.h>
+int main(void)
+{
+    int year;
+    int day,month;
+    printf("Enter the year :");
+    scanf("%d",&year);
+    printf("Enter the month :");
+    scanf("%d",&month);
+    printf("Enter the day :");
+    scanf("%d",&day);
+}
+
+
+
+
+//program to find the next day on the calender//
